fb29532fd34c092b6680769018c8dafe85cc622f
Piraya <pirayatarn@gmail.com>
2023-08-16 15:06:26 +0200

    update build for gh-pages

--- src/components/bioData.js
+++ src/components/bioData.js
@@ -1,5 +1,5 @@
 export const bio = {
 		h2: [ "A graphic designer who codes", "Une graphiste qui code" ],
 		p: [ "Hi, my name is Piraya. I am a graphic designer who believes that everything surrounding us is a source of creativity. Learning programming languages helps me understand a developer's perspective.", 
-			"Polyglotte avec 10 ans d'expérience dans le graphisme. Autonomie et organisée, j'ai une très bonne maîtrise des langages du développement web tels que HTML, CSS et Javascript." ]
+			"Polyglotte avec 10 ans d'expérience dans le graphisme. Autonome et organisée, j'ai une très bonne maîtrise des langages du développement web tels que HTML, CSS et Javascript." ]
 };
