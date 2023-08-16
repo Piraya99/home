7ce316df8052668b6ceb644ee3a7e9b350d32afd
Piraya <pirayatarn@gmail.com>
2023-08-16 14:36:06 +0200

    Fix Back to top bug

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function App() {
   };
 
   return (
-    <div className="App">
+    <div className="App" id="top">
       <header>
         < Navbar french={french} />
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
 	return (
 		<div className={styles.footer}>
 			<div>&copy; Piraya Sanpakij 2023</div>
-			<nav><a href="Header">Back to Top &uarr;</a></nav>
+			<nav><a href="#top">Back to Top &uarr;</a></nav>
 		</div>
 	);
 };
