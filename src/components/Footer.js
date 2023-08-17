bacaa7e4f566196b95409822c8fc91d95cee9204
Piraya <pirayatarn@gmail.com>
2023-08-17 15:43:47 +0200

    Add french version to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from'./Footer.module.css';
 
-export default function Footer() {
+export default function Footer(props) {
 	return (
 		<div className={styles.footer}>
 			<div>&copy; Piraya Sanpakij 2023</div>
-			<nav><a href="#top">Back to Top &uarr;</a></nav>
+			<nav><a href="#top">{props.french ? 'Haut de Page' : 'Back to Top'} &uarr;</a></nav>
 		</div>
 	);
 };
