74814358796fe58fb0cca8ffff86756be91afcfc
Piraya <pirayatarn@gmail.com>
2023-09-12 11:19:37 +0200

    Update responsive in Project Page

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -21,15 +21,15 @@ export default function Carousel(props) {
 	return (
 		<div>
 			<div className={styles.carousel}>
+				<div className={styles.slides}>
+					<img src={props.project.slides[currentIndex]} alt={props.project.name} />
+				</div>
 				{ props.project.slides.length > 1 &&
 					<div className={styles.arrows}>	
 						<img className={styles.arrowLeft} src={ArrowLeftCircleFill} alt="arrow-left" onClick={goToPrevious}/>
 						<img className={styles.arrowRight} src={ArrowRightCircleFill} alt="arrow-right" onClick={goToNext}/>
 					</div>
 				}
-				<div className={styles.slides}>
-					<img src={props.project.slides[currentIndex]} alt={props.project.name} />
-				</div>
 			</div>
 			{ props.project.slides.length > 1 &&
 				<div className={styles.dotsContainer}>
