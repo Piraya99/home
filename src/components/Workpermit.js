eab620a3c3685fc84e0b6846bdcd98837034d3dc
Piraya <pirayatarn@gmail.com>
2024-08-21 11:42:04 +0200

    Replace Workpermit graphics and styling

--- src/components/Workpermit.js
+++ src/components/Workpermit.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import styles from './Workpermit.module.css';
 
-import flagFr from '../img/icon/flag-fr.svg';
-import flagEu from '../img/icon/flag-eu.svg';
-import authorized from '../img/icon/authorized.svg';
+import workpermit from '../img/icon/workpermit.svg';
 
 export default function Workpermit(props) {
 	return (
 		<div className={styles.workpermit}>
-			<div className={styles.workpermitIcons}>
-				<img src={flagFr} alt="flagFr"/>
-				<img src={flagEu} alt="flagEu"/>
-			</div>
-			<img src={authorized} alt="authorized"/>
+			<img src={workpermit} alt="workpermit"/>
 			<p>{props.french ? 'Autorisation de travail avec titre de séjour vie privée et familiale en cours de validité' : 'Work permit in France and EU countries with residence card private and family life'}</p>
 		</div>
 	)
